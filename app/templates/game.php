2ae4f3c594f0c8ae83c8961c149b32086067f0ac
VladimirBG <bukharkinv@gmail.com>
2016-09-27 01:04:18 +0300

    Adds images in top menu, corrects css

--- app/templates/game.php
+++ app/templates/game.php
@@ -1,17 +1,17 @@
 
 <div class="top_menu_for_resurces">
     <div class="resurce_window population_window">
-        <div class="resurce_image"></div>
-        <div class="resurce_count">+235645</div>
+        <div class="resurce_image"><img src="./img/population.png"></div>
+        <div class="resurce_count">100</div>
     </div>
 
     <div class="resurce_window wheat_window">
-        <div class="resurce_image"></div>
-        <div class="resurce_count">+234325</div>
+        <div class="resurce_image"><img src="./img/wheat.png"></div>
+        <div class="resurce_count">2800</div>
     </div>
 
     <div class="resurce_window city_size_window">
-        <div class="resurce_image"></div>
-        <div class="resurce_count">+232353</div>
+        <div class="resurce_image"><img src="./img/land.png"></div>
+        <div class="resurce_count">1000</div>
     </div>
 </div>
