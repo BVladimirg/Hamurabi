259475cc70410b9fc5a2c7b9d309142bc542c97d
VladimirBG <bukharkinv@gmail.com>
2016-09-27 02:38:17 +0300

    Adds a drop-down menu of resource information

--- app/templates/game.php
+++ app/templates/game.php
@@ -3,16 +3,34 @@
     <div class="resurce_window population_window">
         <div class="resurce_image"><img src="./img/population.png"></div>
         <div class="resurce_count">100</div>
+        <div class="information_menu">
+            <h3>Состояние населения на этот год:</h3>
+            <p>-Текущее население города: 2546 чел.</p>
+            <p>-Количество людей, умерших от голода: 25 чел.</p>
+            <p>-Количество людей, приехавших в город : 25000 чел.</p>
+        </div>
     </div>
 
     <div class="resurce_window wheat_window">
         <div class="resurce_image"><img src="./img/wheat.png"></div>
         <div class="resurce_count">2800</div>
+        <div class="information_menu">
+            <h3>Состояние пшеницы на этот год:</h3>
+            <p>-Текущее значение акров пшеницы: 2546 бушелей</p>
+            <p>-Всего пшеницы было собрано за год: 25 бушелей</p>
+            <p>-Всего пшеницы было получено с акра земли: 25 бушелей</p>
+            <p>-Уничтожено крысами: 25000 бушелей</p>
+        </div>
     </div>
 
     <div class="resurce_window city_size_window">
         <div class="resurce_image"><img src="./img/land.png"></div>
         <div class="resurce_count">1000</div>
+        <div class="information_menu">
+            <h3>Состояние города на этот год:</h3>
+            <p>-Город занимает: 2546 акров</p>
+            <p>-Цена одного акра земли в этом году: 25 бушели пшеницы</p>
+        </div>
     </div>
 </div>
 
