b06e3ac402f32e25f31b7d937c88cad7c49777ec
VladimirBG <bukharkinv@gmail.com>
2016-09-26 17:37:27 +0300

    adds css style for top resurses top panel

--- app/templates/game.php
+++ app/templates/game.php
@@ -2,16 +2,16 @@
 <div class="top_menu_for_resurces">
     <div class="resurce_window population_window">
         <div class="resurce_image"></div>
-        <div class="resurce_count">235645</div>
+        <div class="resurce_count">+235645</div>
     </div>
 
     <div class="resurce_window wheat_window">
-        <div class="resurc_image"></div>
-        <div class="resurce_count">234325</div>
+        <div class="resurce_image"></div>
+        <div class="resurce_count">+234325</div>
     </div>
 
     <div class="resurce_window city_size_window">
         <div class="resurce_image"></div>
-        <div class="resurce_count">232353</div>
+        <div class="resurce_count">+232353</div>
     </div>
 </div>
