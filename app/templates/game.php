11c2cc72cb2ceeae488f35b6479aa4b5789fb898
VladimirBG <bukharkinv@gmail.com>
2016-09-26 16:30:13 +0300

    adds top panel for resurces (html)

--- app/templates/game.php
+++ app/templates/game.php
@@ -1,2 +1,17 @@
 
-<p>BLLKBALA</p>
+<div class="top_menu_for_resurces">
+    <div class="resurce_window population_window">
+        <div class="resurce_image"></div>
+        <div class="resurce_count">235645</div>
+    </div>
+
+    <div class="resurce_window wheat_window">
+        <div class="resurc_image"></div>
+        <div class="resurce_count">234325</div>
+    </div>
+
+    <div class="resurce_window city_size_window">
+        <div class="resurce_image"></div>
+        <div class="resurce_count">232353</div>
+    </div>
+</div>
